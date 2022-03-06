1c87a9bb429c04e7747e50771974558573feb254
Tosin Oseni <43161147+tohsin@users.noreply.github.com>
2022-03-06 01:36:16 +0100

    wors

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,TextAreaField
+from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import DataRequired,Length,ValidationError, EqualTo
 from webapp.models import User
-
+from wtforms.widgets import TextArea
 class LoginFormUser(FlaskForm):
     Email = StringField('Email', validators=[DataRequired()])
     Password = PasswordField('Password', validators=[DataRequired()])
@@ -26,19 +27,9 @@ class SignUpFormUser(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
-# class SignUpFormRetailer(FlaskForm):
-#     Email = StringField('Email', validators=[DataRequired(),Length(min=4)])
-#     password1 = PasswordField('Password', validators=[DataRequired(),Length(min=8)])
-#     password2 = PasswordField('Password (Confirm)', validators=[DataRequired(),Length(min=8), EqualTo('password1')])
-#     first_name = StringField('First Name', validators=[DataRequired(),Length(min=4)])
-#     last_name = StringField('Last Name', validators=[DataRequired(),Length(min=4)])
-#     store_name = StringField('Name of Store', validators=[DataRequired(),Length(min=4)])
-#     submit = SubmitField('Sign Up')
-#     def validate_email(self, email):
-#         retailer = Retailer.query.filter_by(email=email.data).first()
-#         if retailer is not None:
-#             raise ValidationError('Please use a different email address.')
-#     def validate_strore_name(self, store_name):
-#         retailer = Retailer.query.filter_by(store_name=store_name.data).first()
-#         if retailer is not None:
-#             raise ValidationError('Please use a different Store Name.')
+class SignUpFormRetailer(FlaskForm):
+    store_name = StringField('Name of Store', validators=[DataRequired(),Length(min=4)])
+    store_icon = FileField(validators=[FileRequired()])
+    store_description = TextAreaField("Store Description" ,validators=[DataRequired(),Length(min=20))
+    submit = SubmitField('Become a Retailer')
+    
